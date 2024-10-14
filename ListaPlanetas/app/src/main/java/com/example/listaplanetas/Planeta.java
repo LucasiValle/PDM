20d41d0da997ff741bc25567d5c893f305a25f03
LucasiValle <lucas.irvalle@gmail.com>
2024-10-14 11:30:52 -0300

    Lista Planetas

---
+++ ListaPlanetas/app/src/main/java/com/example/listaplanetas/Planeta.java
@@ -0,0 +1,11 @@
+package com.example.listaplanetas;
+
+public class Planeta {
+    String nome;
+    Integer foto;
+
+    public Planeta(String nome, Integer foto) {
+        this.nome = nome;
+        this.foto = foto;
+    }
+}
